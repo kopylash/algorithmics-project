e367ac81497d4fea796ba8778350f5351cf7bf09
Artem Zaitsev <minhiriath94@gmail.com>
2017-12-30 00:14:41 +0200

    second game mode

--- app.js
+++ app.js
@@ -41,16 +41,68 @@ class MovablePoint extends Point {
     this.pBestY = y;
   }
 
-  move() {
-    let vx = 2 * Math.random() * (this.pBestX - this.x) + 2 * Math.random() * (gBestX - this.x);
-    vx = Math.abs(this.vx + vx) <= 3 ? this.vx + vx : Math.sign(this.vx + vx) * 3;
-    this.vx = vx;
-    let vy = 2 * Math.random() * (this.pBestY - this.y) + 2 * Math.random() * (gBestY - this.y);
-    vy = Math.abs(this.vy + vy) <= 3 ? this.vy + vy : Math.sign(this.vy + vy) * 3;
-    this.vy = vy;
+  psoMovement(c1, c2, vMax) {
+    let vx = c1 * Math.random() * (this.pBestX - this.x) + c2 * Math.random() * (gBestX - this.x);
+    this.vx = Math.abs(this.vx + vx) <= vMax ? this.vx + vx : Math.sign(this.vx + vx) * vMax;
+    let vy = c1 * Math.random() * (this.pBestY - this.y) + c2 * Math.random() * (gBestY - this.y);
+    this.vy = Math.abs(this.vy + vy) <= vMax ? this.vy + vy : Math.sign(this.vy + vy) * vMax;
 
+    if ((this.x + this.vx) < 800 && (this.x + this.vx) > 0) {
+      this.x += this.vx;
+    }
+    if ((this.y + this.vy) < 600 && (this.y + this.vy) > 0) {
+      this.y += this.vy;
+    }
+  }
+
+  movementPositions() {
+    let coordinates = [-1, 0, 1];
+    let res = [];
+
+    for (let i = 0; i < coordinates.length; i++) {
+      for (let j = 0; j < coordinates.length; j++) {
+        res.push({x: this.x + coordinates[i], y: this.y + coordinates[j]})
+      }
+    }
+    return res.filter((el) => {
+      return !(el.x > 800 || el.y > 600 || el.x < 0 || el.y < 0)
+    })
+  }
+
+  movement(player) {
+    this.distance(player) <= 100 ? this.escapeMovement(player, this.movementPositions()) : this.randomMovement();
+  }
+
+  escapeMovement(player, positions) {
+    let maxDist = 0;
+    let index;
+
+    for (let i = 0; i < positions.length; i++) {
+      let newDist = Point.distanceTwo(player, new Point(positions[i].x, positions[i].y));
+      if (newDist > maxDist) {
+        maxDist = newDist;
+        index = i;
+      }
+    }
+
+    if (positions[index] && positions[index]) {
+      this.x = positions[index].x;
+      this.y = positions[index].y;
+    } else {
+      console.warn(positions);
+    }
+  }
+
+  randomMovement() {
     this.x += this.vx;
     this.y += this.vy;
+
+    if (this.x > 800 || this.x < 0) {
+      this.vx = -this.vx;
+    }
+    if (this.y > 600 || this.y < 0) {
+      this.vy = -this.vy;
+    }
   }
 
   fitness(player) {
@@ -91,6 +143,7 @@ class World {
     this.height = height;
     this.angryPoints = Array.from({length: 100}, () => new MovablePoint(getRandomInt(width), getRandomInt(height)));
     this.player = new Player(width / 2, height / 2);
+    this.mode = 1
   }
 
   simulate() {
@@ -104,8 +157,12 @@ class World {
       gBestX = Infinity;
       gBestY = Infinity;
     });
-    this.angryPoints.forEach(p => p.fitness(this.player));
-    this.angryPoints.forEach(p => p.move());
+    if (this.mode === 1) {
+      this.angryPoints.forEach(p => p.fitness(this.player));
+      this.angryPoints.forEach(p => p.psoMovement(2, 2, 3));
+    } else if (this.mode === 2) {
+      this.angryPoints.forEach(p => p.movement(this.player));
+    }
   }
 
   draw(ctx) {
@@ -145,6 +202,10 @@ let mouseX = 0;
 let mouseY = 0;
 
 canvas.addEventListener('mousemove', setMousePosition, false);
+
+canvas.onmousedown = () => {
+  world.mode === 1 ? world.mode = 2 : world.mode = 1;
+};
 
 function setMousePosition(e) {
   mouseX = e.clientX - canvasPos.x;
